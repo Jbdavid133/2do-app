10266996effd2d1e761fc68d043b05d4aff1f255
Jbdavid133 <jonathandav@wix.com>
2024-03-18 16:34:13 +0200

    (feat): included wix design system into the root html file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import './globals.css';
+import {WixDesignSystemProvider} from '@wix/design-system';
 
-export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <html lang='en'>
-        <body>{children}</body>
+        <head>
+            <title>2Do</title>
+        </head>
+        <body>
+        <WixDesignSystemProvider>
+            {children}
+        </WixDesignSystemProvider>
+        </body>
         </html>
     );
 }
