c38d7c3479460c42dde39028fe0d2985e1a3fb30
Jbdavid133 <jonathandav@wix.com>
2024-03-20 13:46:51 +0200

    (build): added jest setup file for TextEncoder polyfill

--- jest.config.ts
+++ jest.config.ts
@@ -7,22 +7,14 @@ import type {Config} from 'jest';
 import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
-    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
     dir: './',
 });
 
 const config: Config = {
-    // Automatically clear mock calls, instances, contexts and results before every test
-    clearMocks: true,
-
-    // Indicates whether the coverage information should be collected while executing the test
-    collectCoverage: true,
-
-    // The directory where Jest should output its coverage files
-    coverageDirectory: 'coverage',
-
-    // The regexp pattern or array of patterns that Jest uses to detect test files
+    coverageProvider: 'v8',
+    setupFiles: ['./jest.setup.js'],
     testRegex: ['__tests__/.*spec.[jt]sx?$'],
+    testEnvironment: 'jest-environment-jsdom',
 };
 
 export default createJestConfig(config);
