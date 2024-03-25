0645c815fba46475a21d866ab8bcd16f53a63373
Jbdavid133 <jonathandav@wix.com>
2024-03-25 10:26:21 +0200

    (refactor): extracted validators in LoginForm to outside the component function

--- src/app/components/forms/LoginForm/LoginForm.tsx
+++ src/app/components/forms/LoginForm/LoginForm.tsx
@@ -5,13 +5,13 @@ import {FormPropsTypes} from '@/app/components/forms/form-props.types';
 import {Validator, Validators} from '@/app/hooks/useInput/validators';
 import BaseFormField, {BaseFormFieldRef} from '@/app/components/forms/FormField/BaseFormField';
 
+const PASSWORD_VALIDATORS: Validator[] = [Validators.Required('Password must be provided')];
+const USERNAME_VALIDATORS: Validator[] = [Validators.Required('Username must be provided'),
+    Validators.MaxLength(15, 'Username mast not be longer than 15 characters')];
+
 export const LoginForm = ({onSubmit, className}: FormPropsTypes<string>) => {
     const usernameInputRef = useRef<BaseFormFieldRef>(null);
-    const usernameInputValidators: Validator[] = [
-        Validators.Required('Username must be provided'),
-        Validators.MaxLength(15, 'Username mast not be longer than 15 characters')];
     const passwordInputRef = useRef<BaseFormFieldRef>(null);
-    const passwordInputValidators: Validator[] = [Validators.Required('Password must be provided')];
 
     const handleLoginFormSubmit = ($event: FormEvent) => {
         $event.preventDefault();
@@ -29,12 +29,12 @@ export const LoginForm = ({onSubmit, className}: FormPropsTypes<string>) => {
             <Box direction='vertical' gap='10px'>
                 <BaseFormField labelId='UsernameFormField' placeholder='Username' type='text' border='round'
                                size='large'
-                               validators={usernameInputValidators} ref={usernameInputRef}
+                               validators={USERNAME_VALIDATORS} ref={usernameInputRef}
                                prefix={<Input.IconAffix><User></User></Input.IconAffix>}></BaseFormField>
 
                 <BaseFormField labelId='PasswordFormField' placeholder='Password' type='password' border='round'
                                size='large'
-                               validators={passwordInputValidators} ref={passwordInputRef}
+                               validators={PASSWORD_VALIDATORS} ref={passwordInputRef}
                                prefix={<Input.IconAffix><LockLocked></LockLocked></Input.IconAffix>}></BaseFormField>
             </Box>
 
