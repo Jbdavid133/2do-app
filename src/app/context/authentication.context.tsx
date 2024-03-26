b26fc94d5021ad69f8ca876d4931d371c88145a6
Jbdavid133 <jonathandav@wix.com>
2024-03-26 16:11:51 +0200

    (refactor): changed app context to authentication context for context type separation

---
+++ src/app/context/authentication.context.tsx
@@ -0,0 +1,50 @@
+import {createContext, ReactNode, useEffect, useState} from 'react';
+import {SessionStorageKey} from '@/app/constants';
+import {redirect, RedirectType, usePathname} from 'next/navigation';
+import isNil from 'lodash/isNil';
+
+//import {redirect, useRouter} from 'next/navigation';
+
+interface AuthenticationState {
+    isInitialized: boolean;
+    username: string | null;
+    setUsername: (username: AuthenticationState['username']) => void;
+}
+
+const initialState: AuthenticationState = {
+    username: null,
+    isInitialized: false,
+    setUsername: () => {
+    },
+};
+
+export const AuthenticationContext = createContext<AuthenticationState>(initialState);
+
+export const AuthenticationContextProvider = (props: { children: ReactNode | ReactNode[] }) => {
+    const pathname = usePathname();
+    const [isInitialized, setIsInitialized] = useState<boolean>(false);
+    const [username, setUsername] = useState<AuthenticationState['username']>(null);
+
+    useEffect(() => {
+        setUsername(sessionStorage.getItem(SessionStorageKey.USERNAME));
+        setIsInitialized(true);
+    }, []);
+
+    useEffect(() => {
+        if (isInitialized) {
+            if (isNil(username)) {
+                sessionStorage.removeItem(SessionStorageKey.USERNAME);
+
+                if (pathname !== '/') {
+                    redirect('/', RedirectType.replace);
+                }
+            } else if (!isNil(username)) {
+                sessionStorage.setItem(SessionStorageKey.USERNAME, username);
+            }
+        }
+    }, [isInitialized, pathname, username]);
+
+    return <AuthenticationContext.Provider value={{username, setUsername, isInitialized}}>
+        {props.children}
+    </AuthenticationContext.Provider>;
+};
