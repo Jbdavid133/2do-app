f18d212ceecab4541f40f66e63f3f55109f53bca
Jbdavid133 <jonathandav@wix.com>
2024-03-27 10:37:42 +0200

    (refactor): changed the CreateTaskForm components to use the added constants in its template

--- src/app/components/forms/CreateTaskForm/CreateTaskForm.tsx
+++ src/app/components/forms/CreateTaskForm/CreateTaskForm.tsx
@@ -1,11 +1,14 @@
-'use client';
-
 import {Box, Button, FormField, Input, RadioGroup} from '@wix/design-system';
 import {DateChecked, Heading, InfoCircle} from '@wix/wix-ui-icons-common';
 import {CreateTaskFormProps, NewTask, TaskPriorities} from '@/app/components/forms/CreateTaskForm/CreateTaskForm.types';
 import {useFormik} from 'formik';
 import startCase from 'lodash/startCase';
 import {object, string} from 'yup';
+import {CreateTaskDataHook} from '@/app/components/forms/CreateTaskForm/constants/data-hooks.constants';
+import {ValidationMessage} from '@/app/components/forms/CreateTaskForm/constants/validation-messages.constants';
+import moment from 'moment/moment';
+import isNil from 'lodash/isNil';
+import isEmpty from 'lodash/isEmpty';
 
 export const CreateTaskForm = (props: CreateTaskFormProps) => {
     const {setFieldValue, values, touched, errors, handleSubmit, handleChange} = useFormik<NewTask>({
@@ -17,50 +20,55 @@ export const CreateTaskForm = (props: CreateTaskFormProps) => {
         },
         validationSchema: object({
             title: string()
-                .required('Please provide a title to the task')
-                .max(30, 'Title can only contain up to 30 characters'),
+                .required(ValidationMessage.RequiredTitle)
+                .max(30, ValidationMessage.LongTitle),
             description: string()
-                .required('Please provide a description to the task')
-                .max(50, 'Description can only contain up to 50 characters'),
+                .required(ValidationMessage.RequiredDescription)
+                .max(50, ValidationMessage.LongDescription),
         }),
         onSubmit: props.onSubmit
     });
 
-    return <form onSubmit={handleSubmit}>
+    return <form onSubmit={handleSubmit} aria-label='CreateTaskForm'>
         <Box direction='vertical' gap='20px'>
             <FormField labelId='TitleFormField'
-                       status={touched.title && errors.title ? 'error' : undefined}
-                       statusMessage={touched.title && errors.title}>
+                       dataHook={CreateTaskDataHook.TitleFormField}
+                       statusMessage={touched.title && errors.title}
+                       status={touched.title && errors.title ? 'error' : undefined}>
                 <Input placeholder='* Title' type='text' size='large' id='title' name='title'
-                       prefix={<Input.IconAffix><Heading/></Input.IconAffix>}
+                       prefix={<Input.IconAffix><Heading/></Input.IconAffix>} dataHook={CreateTaskDataHook.TitleInput}
                        onChange={handleChange} value={values.title}/>
             </FormField>
 
             <FormField labelId='DescriptionFormField'
+                       dataHook={CreateTaskDataHook.DescriptionFormField}
                        statusMessage={touched.description && errors.description}
                        status={touched.description && errors.description ? 'error' : undefined}>
                 <Input placeholder='* Description' type='text' size='large' id='description' name='description'
                        prefix={<Input.IconAffix><InfoCircle/></Input.IconAffix>}
+                       dataHook={CreateTaskDataHook.DescriptionInput}
                        onChange={handleChange} value={values.description}/>
             </FormField>
 
             <Input placeholder='End Date' type='date' size='large' id='endDate' name='endDate'
                    statusMessage={errors.endDate}
+                   dataHook={CreateTaskDataHook.EndDateInput}
                    status={errors.endDate ? 'error' : undefined}
                    prefix={<Input.IconAffix><DateChecked/></Input.IconAffix>}
-                   onChange={(event) => setFieldValue('endDate', new Date(event.target.value))}
-            />
+                   value={!isNil(values.endDate) ? moment(values.endDate).format('yyyy-MM-DD') : ''}
+                   onChange={(event) => {
+                       setFieldValue('endDate', isEmpty(event.target.value) ? undefined : moment(event.target.value, 'yyyy-MM-DD').toDate());
+                   }}/>
 
-            <FormField labelId='PriorityFormField' id='prioity'>
-                <RadioGroup display='horizontal' selectionArea='always' selectionAreaSkin='outlined' spacing='20px'
-                            selectionAreaPadding='8px 10px' name='prioity' type='default'
-                            fullWidth={true} value={values.priority}
-                            onChange={(priority) => setFieldValue('priority', priority)}>
-                    {TaskPriorities.map(priority =>
-                        <RadioGroup.Radio key={priority} value={priority}>{startCase(priority)}</RadioGroup.Radio>
-                    )}
-                </RadioGroup>
-            </FormField>
+            <RadioGroup display='horizontal' selectionArea='always' selectionAreaSkin='outlined' spacing='20px'
+                        selectionAreaPadding='8px 10px' name='prioity' type='default'
+                        fullWidth={true} value={values.priority} dataHook={CreateTaskDataHook.PriorityRadioGroup}
+                        onChange={(priority) => setFieldValue('priority', priority)}>
+                {TaskPriorities.map(priority =>
+                    <RadioGroup.Radio id={Date.now().toString()} key={priority}
+                                      value={priority}>{startCase(priority)}</RadioGroup.Radio>
+                )}
+            </RadioGroup>
 
             <Button type='submit'>Done</Button>
         </Box>
