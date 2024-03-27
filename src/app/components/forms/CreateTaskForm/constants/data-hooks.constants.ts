0d78c363fa21fef93d4ea0f13717bcc8e4a82378
Jbdavid133 <jonathandav@wix.com>
2024-03-27 13:44:30 +0200

    (refactor): minor changes to CreateTask form validation messages

--- src/app/components/forms/CreateTaskForm/constants/data-hooks.constants.ts
+++ src/app/components/forms/CreateTaskForm/constants/data-hooks.constants.ts
@@ -1,8 +1,8 @@
 export namespace CreateTaskDataHook {
-    export const TitleInput = 'TitleInput';
     export const TitleFormField = 'TitleFormField';
-    export const DescriptionInput = 'DescriptionInput';
     export const DescriptionFormField = 'DescriptionFormField';
+    export const TitleInput = 'TitleInput';
     export const EndDateInput = 'EndDateInput';
+    export const DescriptionInput = 'DescriptionInput';
     export const PriorityRadioGroup = 'PriorityRadioGroup';
 }
--- src/app/components/forms/CreateTaskForm/constants/validation-messages.constants.ts
+++ src/app/components/forms/CreateTaskForm/constants/validation-messages.constants.ts
@@ -1,6 +1,6 @@
 export namespace ValidationMessage {
-    export const LongTitle = 'LongTitle';
-    export const RequiredTitle = 'RequiredTitle';
-    export const LongDescription = 'LongDescription';
-    export const RequiredDescription = 'RequiredDescription';
+    export const RequiredTitle = 'Title must be provided';
+    export const LongTitle = 'Title must not be longer than 30 characters';
+    export const RequiredDescription = 'Description must bew provided';
+    export const LongDescription = 'Description must not be longer than 50 characters';
 }
