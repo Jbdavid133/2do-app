9edae232d946b07a3799d89c4358ab750c44d4af
Jbdavid133 <jonathandav@wix.com>
2024-03-28 15:25:53 +0200

    (feat): added useStorage hook for localStorage handling

---
+++ src/app/hooks/useStorage/useStorage.ts
@@ -0,0 +1,30 @@
+import {useEffect, useState} from 'react';
+import isNil from 'lodash/isNil';
+
+export const useStorage = <T>(key: string) => {
+    const [isInitialized, setIsInitialized] = useState(false);
+    const [value, setValue] = useState<T>();
+
+    useEffect(() => {
+        if (!isInitialized) {
+            const valueJsonString = localStorage.getItem(key);
+
+            isNil(valueJsonString) ? setValue(undefined) : setValue(JSON.parse(valueJsonString));
+
+            setIsInitialized(true);
+        }
+    }, [key, isInitialized]);
+
+    useEffect(() => {
+        if (isInitialized && !isNil(value)) {
+            localStorage.setItem(key, JSON.stringify(value));
+        }
+    }, [isInitialized, key, value]);
+
+    const removeValue = () => {
+        localStorage.removeItem(key);
+        setValue(undefined);
+    };
+
+    return {isInitialized, value, setValue, removeValue};
+};
