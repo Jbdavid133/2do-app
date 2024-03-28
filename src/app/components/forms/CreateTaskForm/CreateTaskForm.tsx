68d93c9a4290010851e3e57d87e97b179f090fa2
Jbdavid133 <jonathandav@wix.com>
2024-03-28 15:46:13 +0200

    (refactor): moved CreateTaskFrom props type to the tsx file and made some minor code styling changes

--- src/app/components/forms/CreateTaskForm/CreateTaskForm.tsx
+++ src/app/components/forms/CreateTaskForm/CreateTaskForm.tsx
@@ -1,6 +1,5 @@
 import {Box, Button, FormField, Input, RadioGroup} from '@wix/design-system';
-import {DateChecked, Heading, InfoCircle} from '@wix/wix-ui-icons-common';
-import {CreateTaskFormProps, NewTask, TaskPriorities} from '@/app/components/forms/CreateTaskForm/CreateTaskForm.types';
+import {Add, DateChecked, Heading, InfoCircle} from '@wix/wix-ui-icons-common';
 import {useFormik} from 'formik';
 import startCase from 'lodash/startCase';
 import {object, string} from 'yup';
@@ -9,9 +8,14 @@ import {ValidationMessage} from '@/app/components/forms/CreateTaskForm/constants
 import moment from 'moment/moment';
 import isNil from 'lodash/isNil';
 import isEmpty from 'lodash/isEmpty';
+import {NewTask, TaskPriorities, TaskPriorityOrder} from '@/app/tasks/types/task.types';
+
+export interface CreateTaskFormProps {
+    onSubmit: (newTask: NewTask) => void;
+}
 
 export const CreateTaskForm = (props: CreateTaskFormProps) => {
-    const {setFieldValue, values, touched, errors, handleSubmit, handleChange} = useFormik<NewTask>({
+    const formik = useFormik<NewTask>({
         initialValues: {
             title: '',
             description: '',
@@ -29,48 +33,48 @@ export const CreateTaskForm = (props: CreateTaskFormProps) => {
         onSubmit: props.onSubmit
     });
 
-    return <form onSubmit={handleSubmit} aria-label='CreateTaskForm'>
+    return <form onSubmit={formik.handleSubmit} aria-label='CreateTaskForm'>
         <Box direction='vertical' gap='20px'>
             <FormField labelId='TitleFormField'
                        dataHook={CreateTaskDataHook.TitleFormField}
-                       statusMessage={touched.title && errors.title}
-                       status={touched.title && errors.title ? 'error' : undefined}>
+                       statusMessage={formik.touched.title && formik.errors.title}
+                       status={formik.touched.title && formik.errors.title ? 'error' : undefined}>
                 <Input placeholder='* Title' type='text' size='large' id='title' name='title'
                        prefix={<Input.IconAffix><Heading/></Input.IconAffix>} dataHook={CreateTaskDataHook.TitleInput}
-                       onChange={handleChange} value={values.title}/>
+                       onChange={formik.handleChange} value={formik.values.title}/>
             </FormField>
 
             <FormField labelId='DescriptionFormField'
                        dataHook={CreateTaskDataHook.DescriptionFormField}
-                       statusMessage={touched.description && errors.description}
-                       status={touched.description && errors.description ? 'error' : undefined}>
+                       statusMessage={formik.touched.description && formik.errors.description}
+                       status={formik.touched.description && formik.errors.description ? 'error' : undefined}>
                 <Input placeholder='* Description' type='text' size='large' id='description' name='description'
                        prefix={<Input.IconAffix><InfoCircle/></Input.IconAffix>}
                        dataHook={CreateTaskDataHook.DescriptionInput}
-                       onChange={handleChange} value={values.description}/>
+                       onChange={formik.handleChange} value={formik.values.description}/>
             </FormField>
 
             <Input placeholder='End Date' type='date' size='large' id='endDate' name='endDate'
-                   statusMessage={errors.endDate}
+                   statusMessage={formik.errors.endDate}
                    dataHook={CreateTaskDataHook.EndDateInput}
-                   status={errors.endDate ? 'error' : undefined}
+                   status={formik.errors.endDate ? 'error' : undefined}
                    prefix={<Input.IconAffix><DateChecked/></Input.IconAffix>}
-                   value={!isNil(values.endDate) ? moment(values.endDate).format('yyyy-MM-DD') : ''}
+                   value={!isNil(formik.values.endDate) ? moment(formik.values.endDate).format('yyyy-MM-DD') : ''}
                    onChange={(event) => {
-                       setFieldValue('endDate', isEmpty(event.target.value) ? undefined : moment(event.target.value, 'yyyy-MM-DD').toDate());
+                       formik.setFieldValue('endDate', isEmpty(event.target.value) ? undefined : moment(event.target.value, 'yyyy-MM-DD').toDate());
                    }}/>
 
             <RadioGroup display='horizontal' selectionArea='always' selectionAreaSkin='outlined' spacing='20px'
                         selectionAreaPadding='8px 10px' name='prioity' type='default'
-                        fullWidth={true} value={values.priority} dataHook={CreateTaskDataHook.PriorityRadioGroup}
-                        onChange={(priority) => setFieldValue('priority', priority)}>
+                        fullWidth={true} value={formik.values.priority} dataHook={CreateTaskDataHook.PriorityRadioGroup}
+                        onChange={(priority) => formik.setFieldValue('priority', priority)}>
                 {TaskPriorities.map(priority =>
-                    <RadioGroup.Radio id={Date.now().toString()} key={priority}
+                    <RadioGroup.Radio id={`${TaskPriorityOrder[priority]}`} key={priority}
                                       value={priority}>{startCase(priority)}</RadioGroup.Radio>
                 )}
             </RadioGroup>
 
-            <Button type='submit'>Done</Button>
+            <Button type='submit' prefixIcon={<Add/>}>Create</Button>
         </Box>
     </form>;
 };
