d59c819cbb38782957ee736602a521b64f846df0
Jbdavid133 <jonathandav@wix.com>
2024-03-31 10:33:13 +0300

    (feat): added data hooks constants file to the TaskCard component

---
+++ src/app/components/TaskCard/constants/data-hooks.constants.ts
@@ -0,0 +1,7 @@
+export namespace TaskCardDataHook {
+    export const Badge = 'TaskCardBadge';
+    export const Header = 'TaskCardHeader';
+    export const EndDateHeading = 'EndDateHeading';
+    export const DeleteButton = 'TaskCardDeleteButton';
+    export const CreationDateHeading = 'CreationDateHeading';
+}
